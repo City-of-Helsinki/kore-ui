a8e55ed181bfbca2fdb70bdf6419d2bbe7e909b2
Lauri Raivio <lauri.raivio@gmail.com>
2015-05-26 00:51:59 +0300

    Move getItemByIdWrapper to utils
    Moves getItemByIdWrapper function to utils so the same
    function can be used by all stores.

--- app/core/utils.js
+++ app/core/utils.js
@@ -19,6 +19,16 @@ function getAddressString(address) {
   return `${street}\, ${municipality}`;
 }
 
+function getItemByIdWrapper(func, items, defaultValue) {
+  return function(itemId) {
+    defaultValue = defaultValue ? defaultValue : {};
+    const item = items[itemId];
+    let newArgs = Array.prototype.slice.call(arguments, 1);
+    newArgs.unshift(item);
+    return _.isEmpty(item) ? defaultValue : func.apply(null, newArgs);
+  };
+}
+
 function getItemForYear(itemList, year) {
   return _.find(itemList, function(item) {
     return item.begin_year <= year;
@@ -32,6 +42,7 @@ function sortByYears(list) {
 export default {
   getAddressArrayFromBuilding: getAddressArrayFromBuilding,
   getAddressString: getAddressString,
+  getItemByIdWrapper: getItemByIdWrapper,
   getItemForYear: getItemForYear,
   sortByYears: sortByYears
 };
